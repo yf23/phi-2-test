79b5ba9267834c31a8eaaaaa02d50383f8d3f214
Yu Fu <yfu@microsoft.com>
2024-01-30 15:07:10 -0800

    Use eos_token

--- phi-2-qa-with-context.py
+++ phi-2-qa-with-context.py
@@ -12,7 +12,7 @@ def read_text(file_path):
 def get_model_output(model, tokenizer, prompt, answer_length=200):
     inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
     length = min(inputs['input_ids'].size()[1] + answer_length, MAX_TOKEN_LENGTH)
-    outputs = model.generate(**inputs, pad_token_id=tokenizer.pad_token_id, max_length=length)
+    outputs = model.generate(**inputs, pad_token_id=tokenizer.eos_token, max_length=length)
     text = tokenizer.batch_decode(outputs)[0]
     return text
 
@@ -21,8 +21,6 @@ def get_model_output(model, tokenizer, prompt, answer_length=200):
 torch.set_default_device("cuda")
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
 
 # User input question
 question = "What is the difference between NC and NCC H100 VMs?"
