481bc5f7c29e73aaede13f6f7b4516153f084659
Yu Fu <yfu@microsoft.com>
2024-01-30 14:41:23 -0800

    add pad_token_id

--- phi-2-qa-with-context.py
+++ phi-2-qa-with-context.py
@@ -11,7 +11,7 @@ def read_text(file_path):
 def get_model_output(model, tokenizer, prompt, answer_length=200):
     inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
     length = min(inputs['input_ids'].size()[1] + answer_length, MAX_TOKEN_LENGTH)
-    outputs = model.generate(**inputs, max_length=length)
+    outputs = model.generate(**inputs, pad_token_id=tokenizer.pad_token_id, max_length=length)
     text = tokenizer.batch_decode(outputs)[0]
     return text
 
@@ -20,6 +20,8 @@ def get_model_output(model, tokenizer, prompt, answer_length=200):
 torch.set_default_device("cuda")
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
 
 # User input question
 question = "What is the difference between Azure NCv5 and NCCv5?"
