14d3a022bfcf77fcb03b6966149f39434af5ffef
Yu Fu <yfu@microsoft.com>
2024-01-30 14:28:22 -0800

    Shorter context

--- phi-2-qa-with-context.py
+++ phi-2-qa-with-context.py
@@ -1,14 +1,23 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+MAX_TOKEN_LENGTH = 2048
+
 def read_text(file_path):
     with open(file_path, "r") as file:
         return file.read()
 
 
-torch.set_default_device("cuda")
+def get_model_output(model, tokenizer, prompt, answer_length=200):
+    inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+    length = min(inputs['input_ids'].size()[1] + answer_length, MAX_TOKEN_LENGTH)
+    outputs = model.generate(**inputs, max_length=length)
+    text = tokenizer.batch_decode(outputs)[0]
+    return text
+
 
 # Load the model and tokenizer
+torch.set_default_device("cuda")
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
@@ -22,15 +31,14 @@ contexts = ncv5_post + '\n' + nccv5_post
 
 # Answer the question without context
 print("Answer without context:")
-inputs = tokenizer(question, return_tensors="pt", return_attention_mask=False)
-outputs = model.generate(**inputs, max_length=200)
-text = tokenizer.batch_decode(outputs)[0]
-print(text)
+prompt = question
+output = get_model_output(model, tokenizer, prompt, answer_length=200)
+answer = output[len(prompt):]
+print(answer)
 
 # Answer the question with context
-print("\nAnswer with context:")
-prompt = """Instruct:Answer based on context:\n\n{contexts}\n\n{question}.\nOutput:"""
-inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
-outputs = model.generate(**inputs, max_length=200)
-text = tokenizer.batch_decode(outputs)[0]
-print(text)
+print("Answer with context:")
+prompt = f"""Instruct:Answer based on context:\n\n{contexts}\n\n{question}\nOutput:"""
+output = get_model_output(model, tokenizer, prompt, answer_length=200)
+answer = output[len(prompt):]
+print(answer)
