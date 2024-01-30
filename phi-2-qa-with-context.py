1edbddc3fdec67d2b16ab3f1631b6eedaa9c9864
Yu Fu <yfu@microsoft.com>
2024-01-30 14:02:42 -0800

    fix

--- phi-2-qa-with-context.py
+++ phi-2-qa-with-context.py
@@ -16,18 +16,21 @@ tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=T
 question = "What is the difference between Azure NCv5 and NCCv5?"
 
 # Load context
-ncv5_post = read_text("text\Azure_nc_h100_v5_blog_post.txt")
-nccv5_post = read_text("text\Azure_nc_h100_v5_blog_post.txt")
+ncv5_post = read_text("./text/Azure_nc_h100_v5_blog_post.txt")
+nccv5_post = read_text("./text/Azure_nc_h100_v5_blog_post.txt")
 contexts = ncv5_post + '\n' + nccv5_post
-print(contexts)
 
 # Answer the question without context
+print("Answer without context:")
 inputs = tokenizer(question, return_tensors="pt", return_attention_mask=False)
 outputs = model.generate(**inputs, max_length=200)
 text = tokenizer.batch_decode(outputs)[0]
+print(text)
 
 # Answer the question with context
+print("\nAnswer with context:")
 prompt = """Instruct:Answer based on context:\n\n{contexts}\n\n{question}.\nOutput:"""
 inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
 outputs = model.generate(**inputs, max_length=200)
 text = tokenizer.batch_decode(outputs)[0]
+print(text)
