4f7d41d246a63ffb5b48344f0dc8fbebe44ca9d1
Yu Fu <yfu@microsoft.com>
2024-01-30 16:26:22 -0800

    Change answer length

--- phi-2-qa-with-context.py
+++ phi-2-qa-with-context.py
@@ -38,17 +38,17 @@ prompt = f'''
 Instruction:Only answer the given question. Stop when you finish answering. Do not start new questions or topics.
 Question:{question}
 Output:'''
-output = get_model_output(model, tokenizer, prompt, answer_length=300)
+output = get_model_output(model, tokenizer, prompt, answer_length=750)
 answer = output[len(prompt):]
 print(answer)
 
 # Answer the question with context
 print(f"\nAnswer with context: {question}")
 prompt = f'''
-Context:{contexts}
 Instruction:Only answer the given question with context provided. Stop when you finish answering. Do not start new questions or topics.
+Context:{contexts}
 Question:{question}
 Output:'''
-output = get_model_output(model, tokenizer, prompt, answer_length=500)
+output = get_model_output(model, tokenizer, prompt, answer_length=750)
 answer = output[len(prompt):]
 print(answer)
