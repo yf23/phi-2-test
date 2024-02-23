bfabe300885832978b01141cec126edec5eba4ed
Yu Fu <yfu@microsoft.com>
2024-02-23 11:33:56 -0800

    change import

--- whisper-benchmark.py
+++ whisper-benchmark.py
@@ -1,11 +1,11 @@
 import gc
 import time
 import torch
+import argparse
 import transformers
-from datasets import load_dataset
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, TextStreamer
-
-BaseStreamer = TextStreamer.__bases__[0]
+from utils import ThroughputStreamer, write_csv_file
+from configs import PERF_BENCHMARK_CONFIG_DICT, LLM_PERF_BENCHMARK_OUTPUT_CSV_COLUMNS
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 
 
 MODEL_NAME = "openai/whisper-large-v3"
@@ -15,52 +15,6 @@ BATCH_SIZE_LIST = [1, 8, 32, 64]
 N_ITERATIONS = 5
 
 
-class ThroughputStreamer(BaseStreamer):
-    def __init__(self):
-        self.first_token_time = -1
-        self.last_token_time = -1
-        self.skip_prompt = True
-        self.n_tokens = 0
-        self.tot_latency = -1
-        self.ftl = -1
-
-    def put(self, value):
-        # Prompt tokens sent first, ignore
-        if self.skip_prompt:
-            self.skip_prompt = False
-            return
-
-        if self.first_token_time == -1:
-            self.first_token_time = time.perf_counter()
-
-        self.n_tokens += len(value.flatten())
-
-    def end(self):
-        self.last_token_time = time.perf_counter()
-
-    def generation_latency(self):
-        return self.last_token_time - self.first_token_time
-
-    def set_latencies(self, pre_generate_start_time, post_generate_end_time):
-        self.ftl = self.first_token_time - pre_generate_start_time
-        self.tot_latency = post_generate_end_time - pre_generate_start_time
-
-    def first_token_latency(self):
-        return self.ftl
-
-    def throughput(self):
-        return self.n_tokens / self.generation_latency()
-
-    def total_latency(self):
-        return self.tot_latency
-
-
-def write_csv_file(line, filepath, append=True):
-    mode = "a" if append else "w"
-    with open(filepath, mode) as f:
-        f.write(line + "\n")
-
-
 def run_model(
     model_name,
     batch_waveform,
@@ -68,10 +22,12 @@ def run_model(
     output_token_length,
 ):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    
+
     # Load the model
     time_start_model_loading = time.perf_counter()
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name, torch_dtype="auto").to(device)
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_name, torch_dtype="auto"
+    ).to(device)
     time_end_model_loading = time.perf_counter()
     time_model_loading = time_end_model_loading - time_start_model_loading
 
