6f79d197002438e2ff54928add8021efba7b35cd
Yu Fu <yfu@microsoft.com>
2024-02-23 16:06:09 -0800

    change batch size

--- configs.py
+++ configs.py
@@ -48,7 +48,7 @@ WHISPER_PERF_BENCHMARK_CONFIG_DICT = {
     "whisper-large-v3-test": {
         "model_name": "openai/whisper-large-v3",
         "output_token_length": [448],
-        "batch_size": [1024],
+        "batch_size": [512],
         "n_iterations": 1,
     },
 }
